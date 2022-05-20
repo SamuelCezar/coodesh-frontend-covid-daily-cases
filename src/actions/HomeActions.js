d98911a0d9270c2074e2d976940d394bc0a343ce
Samuel Chagas <samuelcezar@outlook.com>
2022-05-20 19:49:03 -0300

    Ajustes

--- src/actions/HomeActions.js
+++ src/actions/HomeActions.js
@@ -1,8 +1,20 @@
 import React, {useEffect, useState } from 'react';
 import { ComposableMap, Geographies, Geography} from 'react-simple-maps';
 import axios from 'axios';
-import { Apikey, BASE_URL } from '../service/basicService';
-import {Container, FormControl, FormHelperText, MenuItem, Select} from "@mui/material";
+import { Apikey, BASE_URL } from '../service/URL';
+import {
+    AppBar,
+    Box,
+    Button,
+    Container,
+    FormControl,
+    FormHelperText, IconButton,
+    InputLabel,
+    MenuItem,
+    MenuIcon,
+    Select,
+    Toolbar, Typography
+} from "@mui/material";
 // import { Button, DadosP, DateDados, DivInfo, DivInput, Paragrafo } from './style';
 const _ = require("lodash");
 
@@ -12,21 +24,21 @@ function HomeActions({ setTooltipContent }) {
     const [ infoCase, setInfoCases ] = useState([]);
     const [ nameVariant, setNameVariant ] = useState('Alpha')
     const [ date, setDate ] = useState([])
-    const [ dateSelect, setDateSelect] = useState('2020-05-11')
+    const [ dateSelect, setDateSelect] = useState('11/05/2020')
     const [ dateValue, setDateValue ] = useState(0)
     const [ habilitButton, setHabilitButton ] = useState(true)
 
     const onChange = (e) => {
         setNameVariant(e.target.value)
     }
-    function dateSlectValue(value) {
+    function dateSelectValue(value) {
         setDateValue(value)
         setDateSelect(date[value - 1])
     }
 
     /*Ao usar a database da Supabase descomentar a ApiKey abaixo e configurar o basicService*/
     const getInfoCountry = () => {
-        axios.get(`${BASE_URL}`/*, Apikey*/).then((res) => {
+        axios.get(`${BASE_URL}`, Apikey).then((res) => {
             setInfoCases(res.data);
             dateCorrect();
         })
@@ -77,10 +89,27 @@ function HomeActions({ setTooltipContent }) {
 
 
     return (
-        <Container maxWidth={"80%"}>
-            <div style={{display: "flex", justifyContent:"center", margin: "10px 0",}}>
-                <FormControl sx={{ m: 1, minWidth: 250 }}>
-                    <Select onChange={onChange} label={"Variante"}>
+        <Container maxWidth={"lg"}>
+            <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h4" style={{display: "flex", justifyContent:"center"}}  sx={{ flexGrow: 1 }}>
+                            Casos diários de Covid
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            <div style={{backgroundImage: "linear-gradient(to right, #e7e4e4, #e9dcdc, #ebd4d4, #edcbcb, #eec3c3)"}}>
+
+
+            <div style={{display: "flex",
+                justifyContent:"center",
+                margin: "10px 0",
+                backgroundImage: "linear-gradient(to right, #e7e4e4, #e9dcdc, #ebd4d4, #edcbcb, #eec3c3)"
+            }}>
+                <FormControl color={"primary"} sx={{ m: 1, minWidth: 250 }}>
+                    <InputLabel >Variante</InputLabel>
+                    <Select color={"primary"} onChange={onChange} label={"Variante"}>
                         <FormHelperText>Selecione a variante</FormHelperText>
                         <MenuItem value={nameVariant}>Alpha</MenuItem>
                         {infoCase.slice(1,24).map((dados) => {
@@ -100,27 +129,32 @@ function HomeActions({ setTooltipContent }) {
                 </Button>
             </div>
 
+
             <div style={{display: "flex", justifyContent: "center", margin: "25px 0"}}>
                 <h2 style={{margin: "0 10%"}}>Date: {dateSelect}</h2>
                 <h2 style={{margin: "0 10%"}}>Variant: {nameVariant}</h2>
             </div>
 
-            <DivInput>
-                <DateDados>
+            <div style={{width: "90%",
+                marginLeft: "5%",
+                justifyContent: "center",
+                display: "flex",
+                flexDirection: "column"}}>
+                <div style={{ display: "flex", justifyContent: "space-between"}}>
                     {date.map((dados, index) => {
-                        return <Paragrafo key={index}>{dados}</Paragrafo>
+                        return <p style={{padding: "2% 0", fontSize: "18px", overflow: "hidden"}} key={index}>{dados}</p>
                     })}
-                </DateDados>
+                </div>
                 <input
                     min='1'
-                    max='9'
+                    max='8'
                     value={dateValue}
-                    onChange={(e) => dateSlectValue(Number(e.target.value))}
+                    onChange={(e) => dateSelectValue(e.target.value)}
                     type="range"
                 />
-            </DivInput>
-
-            <div className="mx-1">
+            </div>
+            </div>
+            <div style={{backgroundImage: "linear-gradient(to right, #9296be, #7b7fba, #6468b5, #4d51af, #343aa8)"}} className="mx-1">
                 <ComposableMap data-tip="" projectionConfig={{ scale: 180 }}>
                     <Geographies geography={geoUrl}>
                         {({ geographies }) =>
@@ -140,15 +174,11 @@ function HomeActions({ setTooltipContent }) {
                                                     data = info;
                                                 }
                                             })
-                                            setTooltipContent(`
-                País: ${data.location || infoGeo} |
-                Total de casos: ${totalCases}
-                `);
+                                            setTooltipContent(`País: ${data.location || infoGeo} | Total de casos: ${totalCases}`);
                                         }}
                                         onMouseLeave={() => setTooltipContent("")}
                                         fill={ resultado <= 4 ? '#dbbbb8' : resultado <= 200 ? '#e67a70' : resultado <= 1000 ? '#cf2e1f' : '#8f1106'}
                                         stroke={"black"}
-
                                         style={{hover: {
                                                 fill: "#1B89AE",
                                                 outline: "none"
